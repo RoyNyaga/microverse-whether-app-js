53d3ecf69f2be9b52f1ef78874dd4f9c5812b64a
Roy Nyaga <nyagaandreroy@gmail.com>
2020-01-17 03:32:32 +0100

    fixes

--- src/dom.js
+++ src/dom.js
@@ -3,21 +3,21 @@
 const createWeatherInfoDivC = (feelsLike, humidity, pressure, temp, icon, city, max, min) => {
 	const today = new Date();
 	const dd = today.getUTCDate();
-	const mm = today.getUTCMonth()+1;
+	const mm = today.getUTCMonth() + 1;
 	const yyyy = today.getUTCFullYear();
 
 	const h = today.getUTCHours();
 	const m = today.getUTCMinutes();
 	const s = today.getUTCSeconds();
-	const resultBoard = document.querySelector("#result-board");
-	const content = `<div class="card text-center">
-		<div class="border-bottom p-2 d-flex justify-content-between">
+	const resultBoard = document.querySelector('#result-board');
+	const content = `<div class='card text-center'>
+		<div class='border-bottom p-2 d-flex justify-content-between'>
 			<p>City: ${city}</p>
-			<p><button type="button" class="btn btn-info">C</button> <button type="button" class="btn btn-info">K</button></p>
+			<p><button type='button' class='btn btn-info'>C</button> <button type='button' class='btn btn-info'>K</button></p>
 		</div>
-		<div class="d-flex justify-content-between">
-			<div class="pl-5">
-				<p class="bg-primary">
+		<div class='d-flex justify-content-between'>
+			<div class='pl-5'>
+				<p class='bg-primary'>
 			 	${dd} / ${mm} / ${yyyy}
 				</p>
 				<p>
@@ -27,14 +27,14 @@ const createWeatherInfoDivC = (feelsLike, humidity, pressure, temp, icon, city, 
 					Feels like ${feelsLike} ℃
 				</p>
 			</div>
-			<div class="pr-5">
-				<img class="card-img-top bg-dark" src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="Card image cap">
-				<p class="pt-3">${temp} ℃</p>
+			<div class='pr-5'>
+				<img class='card-img-top bg-dark' src='http://openweathermap.org/img/wn/${icon}@2x.png' alt='Card image cap'>
+				<p class='pt-3'>${temp} ℃</p>
 			</div>
 
 		</div>
 		
-		<div class="card-body border-top d-flex justify-content-between">
+		<div class='card-body border-top d-flex justify-content-between'>
 			<div>
 				<p>humidity</p>
 				<p>${humidity}%</p>
@@ -55,25 +55,25 @@ const createWeatherInfoDivC = (feelsLike, humidity, pressure, temp, icon, city, 
 		</div>
 	</div>`;
 	resultBoard.innerHTML = content;
-}
+};
 
 const createWeatherInfoDivF = (feelsLike, humidity, pressure, temp, icon, city, max, min) => {
 	const today = new Date();
 	const dd = today.getUTCDate();
-	const mm = today.getUTCMonth()+1;
+	const mm = today.getUTCMonth() + 1;
 	const yyyy = today.getUTCFullYear();
 
 	const h = today.getUTCHours();
 	const m = today.getUTCMinutes();
 	const s = today.getUTCSeconds();
-	const resultBoard = document.querySelector("#result-board");
-	const content = `<div class="card text-center">
-		<div class="border-bottom p-2 d-flex justify-content-between">
+	const resultBoard = document.querySelector('#result-board');
+	const content = `<div class='card text-center'>
+		<div class='border-bottom p-2 d-flex justify-content-between'>
 			<p>City: ${city}</p>
 		</div>
-		<div class="d-flex justify-content-between">
-			<div class="pl-5">
-				<p class="bg-primary">
+		<div class='d-flex justify-content-between'>
+			<div class='pl-5'>
+				<p class='bg-primary'>
 			 		${dd} / ${mm} / ${yyyy}
 				</p>
 				<p>
@@ -83,14 +83,14 @@ const createWeatherInfoDivF = (feelsLike, humidity, pressure, temp, icon, city, 
 					Feels like ${feelsLike} ℉
 				</p>
 			</div>
-			<div class="pr-5">
-				<img class="card-img-top bg-dark" src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="Card image cap">
-				<p class="pt-3">${temp} ℉</p>
+			<div class='pr-5'>
+				<img class='card-img-top bg-dark' src='http://openweathermap.org/img/wn/${icon}@2x.png' alt='Card image cap'>
+				<p class='pt-3'>${temp} ℉</p>
 			</div>
 
 		</div>
 		
-		<div class="card-body border-top d-flex justify-content-between">
+		<div class='card-body border-top d-flex justify-content-between'>
 			<div>
 				<p>humidity</p>
 				<p>${humidity}%</p>
@@ -111,6 +111,6 @@ const createWeatherInfoDivF = (feelsLike, humidity, pressure, temp, icon, city, 
 		</div>
 	</div>`;
 	resultBoard.innerHTML = content;
-}
+};
 
 export { createWeatherInfoDivC, createWeatherInfoDivF };
--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
-import {getSearchC, getSearchF} from './api';
-import getWeatherPic from "./weatherPic"
+import { getSearchC, getSearchF } from './api'
+import getWeatherPic from './weatherPic'
 
-// const searchBtnC = document.querySelector("#search-button-Celsius");
-// const searchBtnF = document.querySelector("#search-button-Fahrenheit");
-const overAllDiv = document.querySelector("#over-all-wraper");
-const searchBtns = document.querySelectorAll(".input-group-text");
-
+const overAllDiv = document.querySelector('#over-all-wraper');
+const searchBtns = document.querySelectorAll('.input-group-text');
 
 searchBtns.forEach((btn) => {
-	btn.addEventListener("click", (e) => {
-		let inputField = document.querySelector("#inlineFormInputGroup")
-		let country = inputField.value;
-		let units = e.target.innerHTML;
+	btn.addEventListener('click', (e) => {
+		const inputField = document.querySelector('#inlineFormInputGroup')
+		const country = inputField.value;
+		const units = e.target.innerHTML;
 		getWeatherPic(overAllDiv)
-		if(units === "Celsius"){
+		if(units === 'Celsius'){
 			getSearchC(country);
-		}else{
+		} else {
 			getSearchF(country);
 		}			
-
 	})
-
 }) 
 
-getWeatherPic(overAllDiv)
+getWeatherPic(overAllDiv);

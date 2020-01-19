66347f92aec9d86c891de191cc09b9946b0cc40a
Roy Nyaga <nyagaandreroy@gmail.com>
2020-01-19 16:22:42 +0100

    corrected errors on api.js

--- src/api.js
+++ src/api.js
@@ -4,32 +4,32 @@ import dom from './dom';
 const external = (() => {
   async function getSearchC(country) {
     try {
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${country}&units=metric&APPID=6afc4c4d6d160273a7559007309c6525`, {mode: 'cors'});    
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ country }&units=metric&APPID=6afc4c4d6d160273a7559007309c6525`,{ mode: 'cors' });    
       const weatherData = await response.json();
       const parseData = weatherData.main;
-      let icon = weatherData.weather[0].icon;
-      let city = weatherData.name;
-      dom.createWeatherInfoDivC(parseData.feels_like, parseData.humidity, parseData.pressure, parseData.temp, icon, city, parseData.temp_max, parseData.temp_min);     
-    }catch (error) {
-      alert("Please Enter the Valid name of a City")
+      const icon = weatherData.weather[0].icon;
+      const city = weatherData.name;
+      dom.createWeatherInfoDivC(parseData.feels_like,parseData.humidity,parseData.pressure, 
+        parseData.temp,icon,city,parseData.temp_max,parseData.temp_min);     
+    } catch (error) {
+      alert('Please Enter the Valid name of a City');
     }
-
-  }
+  };
 
   async function getSearchF(country) {
     try {
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${country}&units=imperial&APPID=6afc4c4d6d160273a7559007309c6525`, {mode: 'cors'});    
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ country }&units=imperial&APPID=6afc4c4d6d160273a7559007309c6525`,{ mode: 'cors' });    
       const weatherData = await response.json();
       const parseData = weatherData.main;
-      let icon = weatherData.weather[0].icon;
-      let city = weatherData.name;
-      dom.createWeatherInfoDivF(parseData.feels_like, parseData.humidity, parseData.pressure, parseData.temp, icon, city, parseData.temp_max, parseData.temp_min);     
-    }catch (error) {
+      const icon = weatherData.weather[0].icon;
+      const city = weatherData.name;
+      dom.createWeatherInfoDivF(parseData.feels_like,parseData.humidity,parseData.pressure,
+        parseData.temp,icon,city,parseData.temp_max,parseData.temp_min);     
+    } catch (error) {
       alert("Please Enter the Valid name of a City");
     }
-
-  }
-  return { getSearchC,  getSearchF};
+  };
+  return { getSearchC,  getSearchF };
 })();
 
 export default external;
